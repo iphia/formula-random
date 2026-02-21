b58c9b874ccd373033c9550ee2986a53485bc0e7
iphia <71847143+iphia@users.noreply.github.com>
2026-02-22 07:42:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
  * 예: formulas 폴더에 f01.png 넣었으면 "f01.png" 추가.
  */
 const FORMULAS = [
-  "감자기자력.png",
-  "교차기자력.png",
-  "기동토크가_전부하토크.png",
-  "반사계수.png",
-  "소호리액터.png",
-  "소호리액터_용량.png",
-  "송전전력.png",
-  "정전용량.png",
-  "투과계수.png"
+  "f000.png",
+  "f001.png",
+  "f002.png",
+  "f003.png",
+  "f004.png",
+  "f005.png",
+  "f006.png",
+  "f007.png",
+  "f008.png"
 ];
 
 const LS_KEY = "formula_excluded_set_v1";
